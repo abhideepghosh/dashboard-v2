1e8dd0c45e7b5b40618f97abf28de517bdbf1673
Abhideep Ghosh <109783702+abhideepghosh@users.noreply.github.com>
2023-10-29 14:58:58 +0530

    Bug Fixes

--- general.js
+++ general.js
@@ -1,7 +1,6 @@
 const form = document.querySelector(".handleNext");
 
 form.addEventListener("submit", (e) => {
-  alert("WAIT");
   e.preventDefault();
   window.location.href = "register.html";
 });
