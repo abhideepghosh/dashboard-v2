30a253288c5086a6c0d507b73243ee98da9a558a
Abhideep Ghosh <109783702+abhideepghosh@users.noreply.github.com>
2023-10-29 14:50:10 +0530

    Update general.js

--- general.js
+++ general.js
@@ -1,6 +1,7 @@
 const form = document.querySelector(".handleNext");
 
 form.addEventListener("submit", (e) => {
+  alert("WAIT");
   e.preventDefault();
   window.location.href = "register.html";
 });
