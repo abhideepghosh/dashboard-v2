0c666554a51f144c96513139f87b8f6bd7edf32b
Abhideep Ghosh <109783702+abhideepghosh@users.noreply.github.com>
2023-10-29 11:04:20 +0530

    Workaround HTML

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        general: resolve(__dirname, "general.html"),
+        register: resolve(__dirname, "register.html"),
+      },
+    },
+  },
+});
